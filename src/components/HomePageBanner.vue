f26feb489a0a3ffe7e67a530bad43d1113a23fad
Beaumont Yun <b.yun@hotmail.com>
2021-12-30 14:01:05 +0800

    debugging router-link

--- src/components/HomePageBanner.vue
+++ src/components/HomePageBanner.vue
@@ -1,48 +1,64 @@
 <template>
   <div class="grid grid-cols-3 mb-10">
     <div>
-      <a href="/smart-profile">
-      <img
-        src="../assets/Banner1.svg"
-        id="banner1"
-        alt="Banner 1"
-        class="rounded-t-full w-full"
-      />
-      <p
-        class="text-primary-light text-left border-b-2 border-white w-1/2 ml-3"
-      >
-        smart profile cards
-      </p>
-      </a>
+      <router-link to="/smart-profile">
+        <img
+          src="../assets/Banner1.svg"
+          id="banner1"
+          alt="Banner 1"
+          class="rounded-t-full w-full"
+        />
+        <p
+          class="
+            text-primary-light text-left
+            border-b-2 border-white
+            w-1/2
+            ml-3
+          "
+        >
+          smart profile cards
+        </p>
+      </router-link>
     </div>
     <div>
-        <a href="/governance-token">
-      <img
-        src="../assets/Banner2.svg"
-        id="banner2"
-        alt="Banner 2"
-        class="rounded-t-full w-full"
-      />
-      <p
-        class="text-primary-light text-left border-b-2 border-white w-1/2 ml-3"
+      <router-link to="/governance-token">
+        <img
+          src="../assets/Banner2.svg"
+          id="banner2"
+          alt="Banner 2"
+          class="rounded-t-full w-full"
+        />
+        <p
+          class="
+            text-primary-light text-left
+            border-b-2 border-white
+            w-1/2
+            ml-3
+          "
+        >
+          governance tokens
+        </p></router-link
       >
-        governance tokens
-      </p></a>
     </div>
     <div>
-      <a href="*">
-      <img
-        src="../assets/Banner1.svg"
-        id="banner3"
-        alt="Banner 3"
-        class="rounded-t-full w-full"
-      />
-      <p
-        class="text-primary-light text-left border-b-2 border-white w-1/2 ml-3"
-      >
-        0% commission marketplace
-      </p>
-      </a>
+      <router-link to="*">
+        <img
+          src="../assets/Banner1.svg"
+          id="banner3"
+          alt="Banner 3"
+          class="rounded-t-full w-full"
+        />
+        <p
+          class="
+            text-primary-light text-left
+            border-b-2 border-white
+            w-1/2
+            ml-3
+          "
+        >
+          0% commission marketplace
+        </p>
+      </router-link>
     </div>
   </div>
 </template>
